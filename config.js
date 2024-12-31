35c6b94582dfb18935a5fbb48ef1c1b7df481270
richvybs <156153149+richvybs@users.noreply.github.com>
2024-12-31 11:47:10 +0000

    Update config.js

--- config.js
+++ config.js
@@ -47,8 +47,8 @@ global.isBaileysFail = false
 
 //global.pairingNumber = "" //+254792105520
 global.mods = ['233504759307'] 
-global.prems = ['263719647303']
-global.allowed = ['263719647303']
+global.prems = ['233504759307']
+global.allowed = ['233504759307']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
